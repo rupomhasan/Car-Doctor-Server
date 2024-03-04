e38e015d08c814cd7b6f3f31353ce5bdd2951e7d
rupomhasan <rupom.hasan607299@gmail.com>
2024-03-04 14:11:57 +0600

    backend server for car-machanical-shop

---
+++ index.js
@@ -0,0 +1,39 @@
+const { MongoClient, ServerApiVersion } = require("mongodb");
+// const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+require("dotenv").config();
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const { AsyncLocalStorage } = require("async_hooks");
+const port = process.env.PORT || 2500;
+// middleware
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.r44bh6t.mongodb.net/?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+  }
+}
+run().catch(console.dir);
+
+app.listen(port, () => {
+  console.log(`Example app listen on port : ${port}`);
+});
